fefa41b5e3a4283af31b1b52cb8a1398b1d2f3f7
chaudharyparesh <coding848@gmail.com>
2024-06-15 08:46:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/run-command', (req, res) => {
+    const command = req.body.command;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send(`Error: ${error.message}`);
+        }
+        res.send(`stdout: ${stdout}\nstderr: ${stderr}`);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
